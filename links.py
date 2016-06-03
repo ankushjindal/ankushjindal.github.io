050804dde12436efd2a26e1f7847251d1c665aea
Ankush Jindal <jindal.ankush95@gmail.com>
2016-06-03 23:02:46 +0200

    dry run and arguments error solved

--- links.py
+++ links.py
@@ -9,51 +9,58 @@ try:
   parser = argparse.ArgumentParser(description='Process a link.')
   parser.add_argument('link', action="store", help="Specify the link to add")
   parser.add_argument('-d', '--deploy', action="store_true", default=False, help="To deploy or not") #deploy
-  parser.add_argument('-t', action="store", dest="title", default=False, help="To provide a different tilte than the page header") #title
-  print(parser.parse_args())
+  parser.add_argument('-t', '--title', action="store", dest="title", default=False, help="To provide a different tilte than the page header") #title
+  parser.add_argument('--dry', action="store_true", default=False, help="Dry run, no write") #dry
+  arguments = parser.parse_args()
+  print(arguments)
 except:
   print("Arg error")
   sys.exit(0)
 
 try:
+  link = arguments.link
   r = requests.get(link)
   tree = fromstring(r.content)
+  title = arguments.title
   if not title:
     title = tree.findtext('.//title')
     if not title:
         title = link
   timestr = strftime("%Y-%m-%d", gmtime())
+  new = "<br><i>" + timestr + "</i> <a href='" + link + "'>" + title + "</a>\n"
   print("Link fetched")
 except:
   print("Link error")
   sys.exit(0)
 
-try:
-  file = open("index.html","r+")
-  data = file.read()
-  new = "<br><i>" + timestr + "</i> <a href='" + link + "'>" + title + "</a>\n"
-  print(new)
-  file.write(new)
-  file.close()
-  print("File edited")
-except:
-  print("File error")
-  sys.exit(0)
+if not arguments.dry:
+  try:
+    file = open("index.html","r+")
+    data = file.read()
+    print(new)
+    file.write(new)
+    file.close()
+    print("File edited")
+  except:
+    print("File error")
+    sys.exit(0)
 
-try:
-  if deploy:
-    commitstr = "'Added link " + str(link)+"'"
-    os.system("git commit -a -m "+commitstr)
-    print("deploying "+commitstr)
-    passfile = open("pass.txt","r")
-    passdata = passfile.read().split()
-    username = passdata[0]
-    password = passdata[1]
-    passfile.close()
-    print("getting pass")
-    pushstr = "https://"+str(username)+":"+str(password)+"@github.com/ankushjindal/ankushjindal.github.io.git"
-    print("pushing")
-    os.system("git push -u "+pushstr+" master")
-except:
-  print("Git/Pass error")
-  sys.exit(0)
+  try:
+    if deploy:
+      commitstr = "'Added link " + str(link)+"'"
+      os.system("git commit -a -m "+commitstr)
+      print("deploying "+commitstr)
+      passfile = open("pass.txt","r")
+      passdata = passfile.read().split()
+      username = passdata[0]
+      password = passdata[1]
+      passfile.close()
+      print("getting pass")
+      pushstr = "https://"+str(username)+":"+str(password)+"@github.com/ankushjindal/ankushjindal.github.io.git"
+      print("pushing")
+      os.system("git push -u "+pushstr+" master")
+  except:
+    print("Git/Pass error")
+    sys.exit(0)
+else:
+  print(new)
